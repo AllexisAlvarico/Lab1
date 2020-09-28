55f840c741610cafca56f339a3ce9883799df9a8
AllexisAlvarico <c00242855@itcarlow.ie>
2020-09-28 19:56:45 +0100

    Lab 1 added the movement

---
+++ Assets/Scenes/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("d")|| Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+    }
+}
+
